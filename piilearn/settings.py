64c61f59bab80b4258b7376a2214c7f9deea1477
Elias Chikwanda <chikwandaelias@gmail.com>
2023-05-10 10:35:17 +0000

    Changed the STATIC_URL and created the STATIC_ROOT

--- piilearn/settings.py
+++ piilearn/settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'media/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/pilearn01/Pi-Learn/static/'
 
 
 
