6e3a423129f1ef1cc2d510fd480fd502187bd863
Elias Chikwanda <chikwandaelias@gmail.com>
2023-06-08 13:38:59 +0200

    Styling the policy pages

--- examsolution/urls.py
+++ examsolution/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('terms_of_use/', views.terms_of_use, name='terms_of_use'),
     
     path('login/',CustomLoginView.as_view(),name='login'),
-    path('logout/',LogoutView.as_view(next_page='examsolution:login'),name='logout'),
+    path('logout/',LogoutView.as_view(next_page='examsolution:home'),name='logout'),
     path('register/',RegisterPage.as_view(),name='register'),
     
     path('my_to_do_list/',UserTaskList.as_view(),name='my_to_do_list'),
