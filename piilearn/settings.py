e4b4725f65f044a43dd86c4fe85497b3ad9d4fc5
Elias Chikwanda <chikwandaelias@gmail.com>
2023-06-11 14:33:53 +0200

    Disabling other hosts

--- piilearn/settings.py
+++ piilearn/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-ktbd414+vzeg81t+!ca0)mznwdyjnegocvnyu3$fb!u#3f&(6z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['pilearn01.pythonanywhere.com','127.0.0.1','www.piilearn.com']
+ALLOWED_HOSTS = ['www.piilearn.com']
 
 
 # Application definition
